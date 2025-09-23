ca52bd2451224d722ddbf67832e75e03f43a9dc6
Gabriel Soares <gabrielsoaresm94@gmail.com>
2025-09-23 19:30:16 -0300

    feat: update project

--- src/publishers/UserPublisher.ts
+++ src/publishers/UserPublisher.ts
@@ -1,7 +1,17 @@
-import { RabbitMQProvider } from "../providers/rabbitmq/RabbitMQProvider";
+import { IRabbitMQProvider } from "../providers/rabbitmq/IRabbitMQProvider";
 
-export class UserPublisher {
-  static async userCreated(user: any) {
-    await RabbitMQProvider.publish("user_created", user);
+interface IUserPublisher {
+  userCreated(user: any): Promise<void>;
+}
+
+export class UserPublisher implements IUserPublisher {
+  private messaging: IRabbitMQProvider;
+
+  constructor(messaging: IRabbitMQProvider) {
+    this.messaging = messaging;
+  }
+
+  async userCreated(user: any) {
+    await this.messaging.publish("user_created", user);
   }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,12 @@
+import { RabbitMQProvider } from "../providers/rabbitmq/RabbitMQProvider";
 import { UserPublisher } from "../publishers/UserPublisher";
 
 export class UserService {
   async create(data: any) {
+    const rabbitMQProvider = new RabbitMQProvider();
+    const publisher = new UserPublisher(rabbitMQProvider);
     const user = { id: Date.now(), ...data };
-
-    // publica evento de forma desacoplada
-    await UserPublisher.userCreated(user);
-
+    await publisher.userCreated(user);
     return user;
   }
 }
